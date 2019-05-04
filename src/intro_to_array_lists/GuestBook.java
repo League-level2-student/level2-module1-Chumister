149b398b0c9c8e3122ce498b688f106d7b1dfcc1
league <league@wts-im-17.attlocal.net>
2019-05-04 15:57:53 -0700

    sdf

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -5,6 +5,7 @@ public class GuestBook {
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
+	
 	// Guest #1: Bob Banders
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
